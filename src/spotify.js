77ec9919296813d14fb3ac1d982b671206073253
Huỳnh Dũng <@huynhdungfed>
2021-04-02 14:39:45 +0700

    v1

---
+++ src/spotify.js
@@ -0,0 +1,29 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "83072b1bc5c44e13b1b220a776091897";
+const redirectUri = "http://localhost:3000/";
+// eslint-disable-next-line no-unused-vars
+var client_secret = 'bd5dfd72481740ed82728034620885ce'; // Your secret
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20")}&response_type=token&show_dialog=true`;
